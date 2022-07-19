145b0c581136e1257fda18d8a7d06be787896279
theerudito <erudito.tv@gmail.com>
2022-07-19 12:14:19 -0500

    v1

--- src/Components/helper/api.jsx
+++ src/Components/helper/api.jsx
@@ -4,10 +4,8 @@ const url =
   "https://ecovitali.presttoapp.net/Puertto/PostCalidad/Api/api/Login";
 
 export const APIUSER = async ({ data }) => {
-  
   try {
     const res = await axios.get(url, data);
-
     return res.data;
   } catch (error) {
     console.log(error);
